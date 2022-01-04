d0a438095f52fca09beea40a3d2ab8e6b4629e8c
Якупов Радик <kapysta-xxl@mail.ru>
2022-01-04 10:46:44 +0300

    edit routes

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -19,5 +19,10 @@ export const routes = [
         path: CREATE_ROUTE,
         Component: CreatePage,
         title: 'Create'
+    },
+    {
+        path: '*',
+        Component: BoardsPage,
+        title: 'issue Boards'
     }
 ]
